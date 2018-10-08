f79dc93c9ea64a54b5284489ce03594dde725e07
Vadim Pashaev <vmp@live.ru>
2018-10-09 03:50:42 +0400

    Updates

---
+++ precache-manifest.f2bc4dad7dae2062c4116609b5cbfa15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acf7d949d256fef75ae1",
+    "url": "/react-day4-new-color-sections/static/js/runtime~main.acf7d949.js"
+  },
+  {
+    "revision": "01ca738accfc172a32ce",
+    "url": "/react-day4-new-color-sections/static/js/main.01ca738a.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-day4-new-color-sections/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "01ca738accfc172a32ce",
+    "url": "/react-day4-new-color-sections/static/css/main.b1cc3f0d.chunk.css"
+  },
+  {
+    "revision": "b847edafd22992f7ea05021ab8e243d7",
+    "url": "/react-day4-new-color-sections/index.html"
+  }
+];
